9044ba96b35e84c2d90e37903b5c20d2fa6ec24f
ShiroNekuro <mf.rahman98@gmail.com>
2025-06-17 12:51:27 +0700

    adding bootstrap

--- taskhub/src/main/java/com/shiro/Controllers/HomeController.java
+++ taskhub/src/main/java/com/shiro/Controllers/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class HomeController {
+    
     public String welcome(){
         return "index";
     }    
